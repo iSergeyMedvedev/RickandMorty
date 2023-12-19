4beeff20662d4fe388187b56c862ff54c39a2977
Sergey Medvedev <devgod@mail.ru>
2023-12-19 17:26:30 +0300

    commit

---
+++ Rick and Morty/Models/APIResponseTypes/RMGetCharactersResponse.swift
@@ -0,0 +1,20 @@
+//
+//  RMGetCharactersResponse.swift
+//  Rick and Morty
+//
+//  Created by Sergey Medvedev on 19.12.2023.
+//
+
+import Foundation
+
+struct RMGetAllCharactersResponse: Codable {
+    struct Info: Codable {
+        let count: Int
+        let pages: Int
+        let next: String?
+        let prev: String?
+    }
+    
+    let info: Info
+    let results: [RMCharacter]
+}
